3e4b64e5c28ca8f64a8fe315e062f9b2055c9705
pvccortez <pvccortez@gmail.com>
2023-02-25 06:18:07 -0800

    Trying to fix blank page issue
    Adding router to fix the "You need to enable Javascript to run this app" issue.

--- src/index.js
+++ src/index.js
@@ -47,7 +47,8 @@ if(!hasData)
 //                               }
 //                             ]}
 
-
+// Possible fix to "You need to enable Javascript to run this app" issue, 
+const router = createBrowserRouter([{ path: "/", element: <div>Test page</div> }]); /*Delete if it doesn't work*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
